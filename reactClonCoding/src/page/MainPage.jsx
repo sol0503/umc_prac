e83e9ec2cd626cb75d82df4dbb6b533f16ec228d
leesol <jbkljo@naver.com>
2023-04-05 18:07:35 +0900

    feat: 보이는 header 완성(구체적인거 다듬기)

--- reactClonCoding/src/page/MainPage.jsx
+++ reactClonCoding/src/page/MainPage.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as Logo } from "../svg/logo.svg";
 import { ReactComponent as Lan } from "../svg/lan.svg";
 import { ReactComponent as User } from "../svg/user.svg";
 import { ReactComponent as Person } from "../svg/person.svg";
+import { ReactComponent as Search } from "../svg/search.svg";
 const MainPage = () => {
   return (
     <div className="mainPage">
@@ -12,7 +13,26 @@ const MainPage = () => {
             <Logo />
           </div>
           <div className="search">
-            <button className="searchBtn"></button>
+            <button className="searchBtn">
+              <button className="btn">어디든지</button>|
+              <button className="btn">언제든 일주일</button>|
+              <button className="btn">게스트 추가</button>
+              <button
+                style={{
+                  height: "32px",
+                  width: "32px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#FF385C",
+                  color: "white",
+                  border: "none",
+                  borderRadius: "50%",
+                }}
+              >
+                <Search />
+              </button>
+            </button>
           </div>
           <div className="headRight">
             <button className="yours" style={{ width: "232px" }}>
